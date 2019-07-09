5da649b0110b46da6e35471df793adb29c0ac35c
MohanRaj Raman <mohan@qoruz.com>
2019-07-09 15:06:23 +0530

    Added Account , Subscriptions and Roles Modules

---
+++ app/Subscriptions/Models/Pricing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Subscriptions\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pricing extends Model
+{
+    //
+}
---
+++ app/Users/Models/UserInvite.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Users\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserInvite extends Model
+{
+    //
+}
