90d9a0b272bd18e59eee3d94875c6a098e76d322
Rakesh <info.rakesh1993@gmail.com>
2019-07-15 14:39:28 +0530

    added new functions in Subscribable, migrations updated

--- app/Subscriptions/Traits/Subscribable.php
+++ app/Subscriptions/Traits/Subscribable.php
@@ -77,4 +77,119 @@ trait Subscribable {
            }
         }
      }
+
+     /**
+      * Check SUbscription for ProfileViews
+      * @return bool
+      */
+      public function canViewProfile()
+      {
+         $subscription = $this->subscription();
+         return ($subscription->profiles_count) ? true : false ;
+      }
+
+      /**
+       * Check Subscription for Report Refresh
+       * @return bool
+       */
+      public function canReportRefresh()
+      {
+         $subscription = $this->subscription();
+         return ($subscription->report_refresh) ? true : false ;
+      }
+
+      /**
+       * Check Subscription for creating Report
+       * @return bool
+       */
+      public function canCreateReport()
+      {
+         $subscription = $this->subscription();
+         return ($subscription->reports_count) ? true : false ;
+      }
+
+      /**
+       * Check Subscrition for creating new plan
+       * @return bool
+       */
+      public function canCreatePlan()
+      {
+         $subscription = $this->subscription();
+         return ($subscription->plans_count) ? true : false ;
+      }
+
+      /**
+       * Check Subscription for creating new User
+       * @return bool
+       */
+      public function canCreateUser()
+      {
+         $subscription = $this->subscription();
+         return ($subscription->users_count) ? true : false ;
+      }
+
+      /**
+       * Check Subscription for creating new Brand
+       * @return bool
+       */
+      public function canUseBrand()
+      {
+         $subscription = $this->subscription();
+         return ($subscription->brands_count) ? true : false ;
+      }
+
+      /**
+       * Decrement the Profile View of subscription
+       * @return bool
+       */
+      public function decreaseProfileView($count = 1)
+      {
+         $subscription = $this->subscription();
+         $subscription->decrement('profile_views', $count);
+         return $subscription;
+      }
+
+      /**
+       * Decrement the Report Count of subscription
+       * @return bool
+       */
+      public function decreaseReportCount($count = 1)
+      {
+         $subscription = $this->subscription();
+         $subscription->decrement('reports_count', $count);
+         return $subscription;
+      }
+
+      /**
+       * Decrement the Plan Count of subscription
+       * @return bool
+       */
+      public function decreasePlanCount($count = 1)
+      {
+         $subscription = $this->subscription();
+         $subscription->decrement('plans_count', $count);
+         return $subscription;
+      }
+
+      /**
+       * Decrement the Brand Count of subscription
+       * @return bool
+       */
+      public function decreaseBrandCount($count = 1)
+      {
+         $subscription = $this->subscription();
+         $subscription->decrement('brands_count', $count);
+         return $subscription;
+      }
+
+      /**
+       * Decrement the User Count of subscription
+       * @return bool
+       */
+      public function decreaseUserCount($count = 1)
+      {
+         $subscription = $this->subscription();
+         $subscription->decrement('users_count', $count);
+         return $subscription;
+      }
 }
