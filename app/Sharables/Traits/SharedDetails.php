fc5cf2a2ffb030c784cf62ed14507381cc1453c7
Rakesh <info.rakesh1993@gmail.com>
2019-07-19 17:40:53 +0530

    added Trait file

---
+++ app/Sharables/Traits/SharedDetails.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Sharables\Traits;
+
+use App\Sharables\Models\Sharable as Sharable;
+
+
+trait SharedDetails {
+
+    
+    public function sharedWithMe()
+    {
+       return $this->hasMany(Sharable::class, 'share_to');
+    }
+
+    public function sharedBy()
+    {
+        return $this->hasMany(Sharable::class,'share_by');
+    }
+
+    public function sharedPlans()
+    {
+        return $this->sharedBy()->where('sharable_type','App\Plan\Models\Plan')->get();
+    }
+
+}
